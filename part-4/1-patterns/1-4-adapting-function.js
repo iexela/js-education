79e498cc0e2b270e0e9684e0f0b4bd282447c9c0
Aleksey Mitar <aleksey.mitar@softeq.com>
2015-12-21 10:16:45 +0300

    Added one more pattern

---
+++ part-4/1-patterns/1-4-adapting-function.js
@@ -0,0 +1,62 @@
+
+function setProp(obj, name, value) {
+  obj[name] = value;
+}
+
+function getProp(obj, name) {
+  return obj[name];
+}
+
+function setProps(obj, dict) {
+  var n;
+
+  for(n in dict) {
+    if (dict.hasOwnProperty(n)) {
+      setProp(obj, n, dict[n]);
+    }
+  }
+}
+
+var jam = {
+  name: "Jam",
+  age: 8
+};
+
+setProp(jam, "age", 9);
+getProp(jam, "age");
+
+setProps(jam, {
+  name: "Unchida",
+  age: 10
+});
+
+/******************************************************************************/
+
+function prop(obj, name, value) {
+  switch (typeof name) {
+  case "undefined":
+    return obj[name];
+  case "object":
+    Object.keys(name).forEach(function (n) {
+      prop(obj, n, name[n]);
+    });
+    break;
+  default:
+    obj[name] = value;
+    break;
+  }
+}
+
+prop(jam, age, 9);
+prop(jam, "age");
+prop(jam, {
+  name: "Unchida",
+  age: 10
+});
+
+/******************************************************************************/
+
+if (typeof jQuery !== "undefined") {
+  jQuery("div").attr("tabindex");
+  jQuery("div").attr("tabindex", "0");
+}
