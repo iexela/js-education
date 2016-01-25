165e5c4ec9749aa9312cfcff09297afb09330add
Aleksey Mitar <aleksey.mitar@softeq.com>
2016-01-25 11:57:52 +0300

    Simplified code

--- part-3/2-prototype-chain/2-3-chain.js
+++ part-3/2-prototype-chain/2-3-chain.js
@@ -3,23 +3,13 @@ function Pet(name, age) {
   this.name = name;
   this.age = age;
 }
-
+var obj = {};
+var obj = new Object({});
+Object.prototype;
 //Pet.prototype is an empty object (new Object())
 
 Pet.prototype.feed = function (what) {
   console.log("%s eats %s", this.name, what);
-};
-
-//Cat prototypically inherits from Pet
-function Cat(name, age) {
-  Pet.call(this, name, age);
-}
-
-Cat.prototype = new Pet();
-Cat.prototype.constructor = Cat;
-
-Cat.prototype.love = function (what) {
-  return this.name + " loves self rather than " + what;
 };
 
 //Dog prototypically inherits from Pet
@@ -28,31 +18,15 @@ function Dog(name, age) {
 }
 
 Dog.prototype = new Pet();
-Dog.prototype.constructor = Dog;
-
-Dog.prototype.love = function (what) {
-  return this.name + " loves food rather than " + what;
-};
+// Dog.prototype.constructor = Dog;
+//
+// Dog.prototype.love = function (what) {
+//   return this.name + " loves food rather than " + what;
+// };
 
 /******************************************************************************/
 var jam = new Dog("Jam", 8);
-var bagira = new Cat("Bagira", 7);
-
-console.log(jam.love("you")); //Jam loves food rather than you (found on Dog)
-console.log(bagira.love("you")); //Bagira loves self rather than you (found on Cat)
-
-/*
- * love is searched on:
- * jam[.love]           : jam created by Dog              : (not found)
- * Dog.prototype[.love] : Dog.prototype created by Pet    : (found)
- * Pet.prototype        : Pet.prototype created by Object : (search does not come here)
- * Object.prototype     :                                 : (search does not come here)
- * STOP (found on Dog.prototype)
- */
-
-/******************************************************************************/
-jam.feed("meet"); //Jam eats meet (found on Pet)
-bagira.feed("meet"); //Bagira eats meet (found on Pet)
+jam.feed("meet"); //Bagira eats meet (found on Pet)
 
 /*
  * feed is searched on:
@@ -77,7 +51,7 @@ console.log(jam.lastName);
 
 /******************************************************************************/
 
-console.log("love" in jam); //true
+// console.log("love" in jam); //true
 console.log("feed" in jam); //true
 
 console.log(jam instanceof Dog); //true
